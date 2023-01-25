e1831108b0a0d5b255a1b6a6d0d9cb330e768d72
Param290601 <paramt.mobio@gmail.com>
2023-01-25 11:46:01 +0530

    updated library

--- library.py
+++ library.py
@@ -18,28 +18,29 @@ zlib.decompress(t)
 # date
 from datetime import date
 now = date.today()
-now
-now.strftime("%m-%d-%y. %d %b %Y is a %A on the %d day of %B.")
+print(now)
+print(now.strftime("%m-%d-%y. %d %b %Y is a %A on the %d day of %B."))
 # dates support calendar arithmetic
 birthday = date(1964, 7, 31)
 age = now - birthday
+print(age)
 age.days
 
 # math
-math.cos(math.pi / 4)
-math.log(1024, 2)
+print(math.cos(math.pi / 4))
+print(math.log(1024, 2))
 
 # statistics
 data = [2.75, 1.75, 1.25, 0.25, 0.5, 1.25, 3.5]
-statistics.mean(data)
-statistics.median(data)
-statistics.variance(data)
+print(statistics.mean(data))
+print(statistics.median(data))
+print(statistics.variance(data))
 
 # random
-random.choice(['apple', 'pear', 'banana'])
-random.sample(range(100), 10)   # sampling without replacement
-random.random()    # random float
-random.randrange(6)    # random integer chosen from range(6)
+print(random.choice(['apple', 'pear', 'banana']))
+print(random.sample(range(100), 10))   # sampling without replacement
+print(random.random())    # random float
+print(random.randrange(6))    # random integer chosen from range(6)
 
 # glob
 print(glob.glob('*.py'))
@@ -49,4 +50,4 @@ print(os.getcwd())      # Return the current working directory
 os.system('mkdir abc')   # Run the command mkdir in the system shell
 
 
-help(os)
+# help(os)
